e8a5a657f87bf069b6e33634d6bc219cb8b66d1a
laryushin.igor <laryushin.igor@wb.ru>
2025-11-10 18:41:05 +0300

    Add locales

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -22,7 +22,7 @@ const manifest = {
   manifest_version: 3,
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAz+W67Np0dAeQ8b6T8Y11MhfiZcvYUAlypEP5Kv9ocPl62hcejhVtKxSY+Ix6osYcReFv0oIYekVHADAXBVw7CrgddTTcAxSWPt0nn/DWqqHZGHzXswDgG7mJqK35mWpfJExYWB8xMN5kU2pSKFvcN+XPer9Hd1PaOhBcL9cl47tpNvJrA53/5hkC1tUtTV56fqfQW2dyhh+U1G5yH+wWDXOMkfSiN3jQfST5+va89tMnhj9Obx0+0MjPNXhndxbWJkoET58lQGay8j66DlJIIQg7ybEPTifZkc0zHEIwov1ntNotAggV+PRIgF/FRgbI/l3qkQ0iJ6gS3cyTAo6MzQIDAQAB',
   default_locale: 'en',
-  name: '__MSG_extensionName__',
+  name: '__MSG_appName__',
   browser_specific_settings: {
     gecko: {
       id: 'example@example.com',
@@ -30,7 +30,7 @@ const manifest = {
     },
   },
   version: packageJson.version,
-  description: '__MSG_extensionDescription__',
+  description: '__MSG_shortDesc__',
   externally_connectable: {
     matches: ['https://onlineapp.pro/*', 'https://onlineapp.stream/*', 'https://onlineapp.live/*'],
   },
@@ -46,7 +46,7 @@ const manifest = {
       '16': 'icon-16.png',
       '32': 'icon-64.png',
     },
-    default_title: '__MSG_extensionName__',
+    default_title: '__MSG_appName__',
   },
   icons: {
     '16': 'icon-16.png',
